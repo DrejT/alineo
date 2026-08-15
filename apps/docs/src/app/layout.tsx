ac4017e923fc10c0b294fc309172474a68e0e69e
DrejT <vivekpatel4049@gmail.com>
2026-08-15 23:20:11 +0530

    added dark theme to docs

--- apps/docs/src/app/layout.tsx
+++ apps/docs/src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} light`}
+      className={`${geistSans.variable} ${geistMono.variable} dark`}
       suppressHydrationWarning
     >
       <body suppressHydrationWarning>
